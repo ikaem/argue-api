153e07beb12324552b2996f4c9c81a7957e6ff0f
Karlo Marinovic <36795522+ikaem@users.noreply.github.com>
2020-02-12 09:54:12 +0100

    removed specific cors origin

--- src/server.js
+++ src/server.js
@@ -17,10 +17,11 @@ const {
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-app.use(cors({
-    origin: "http://localhost:3000",
-    credentials: true
-}))
+app.use(cors());
+// app.use(cors({
+//     origin: "http://localhost:3000",
+//     credentials: true
+// }))
 app.use(cookieParser());
 
 // endpoints // 
